8bf1909725870a149a2f282f31945cde87bcaff0
Kaavya Indhu <kaavyaindhu06@gmail.com>
2026-05-27 21:35:42 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        hashSet= { "}" :"{", "]":"[", ")":"("}
+        for c in s: 
+            if c in hashSet :
+                if stack and stack[-1] == hashSet[c] :
+                    stack.pop()
+                else:
+                    return False 
+                
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+                
+     
